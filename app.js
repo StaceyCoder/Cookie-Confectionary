5daf31cbe3b8ce7ecff82789fc98db468ed3cf50
StaceyCoder <Stacey.Beason@gmail.com>
2018-06-03 14:16:34 -0400

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const ejs = require('ejs')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
